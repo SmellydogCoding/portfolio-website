e180c80c22e61def9f253aa2a2629cfa0ed08c89
SmellydogCoding <SmellydogCoding@gmail.com>
2018-08-12 17:37:55 -0400

    tweak footer, add cache control

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  v-footer(app color="primary")
+  v-footer(color="primary")
       p.ml-2.mb-0.pr-2.white--text &copy; 2018 #[span.blue--text--darken-4 Smellydog Coding]
       v-spacer
       v-btn.white--text(v-for="icon in icons" :key="icon.index" icon :href="icon.link" target="new")
